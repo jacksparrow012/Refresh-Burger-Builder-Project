5e525d35eb451848c914d7742304ba4aeaeee506
Nazrul islam <jacksparrow.alex@gmail.com>
2021-02-28 19:33:36 +0600

    price showing on card when user add/remove ingredient

--- src/Components/BurgerBuilder/BugerBuilder.js
+++ src/Components/BurgerBuilder/BugerBuilder.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import Burger from "./Burger/Burger"
 import Controls from "./Controls/Controls"
+const INGREDIENT_PRICES = {
+    salad: 20,
+    cheese: 40,
+    meat: 90
+}
 class BugerBuilder extends Component {
+
     state = {
         ingredients: [
             { type: "salad", amount: 0 },
             { type: "cheese", amount: 0 },
             { type: "meat", amount: 0 }
-        ]
+        ],
+        totalPrice: 80,
     }
     addIngredientHandle = type => {
         const ingredients = [...this.state.ingredients]
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type]
         for (let item of ingredients) {
             if (item.type === type) item.amount++
-            this.setState({ ingredients: ingredients })
+            this.setState({ ingredients: ingredients, totalPrice: newPrice })
         }
     }
     removeIngredientHandle = type => {
         const ingredients = [...this.state.ingredients]
+        const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type]
         for (let item of ingredients) {
             if (item.type === type) {
                 if (item.amount <= 0) return;
                 item.amount--
             }
-            this.setState({ ingredients: ingredients })
+            this.setState({ ingredients: ingredients, totalPrice: newPrice })
         }
     }
     render() {
@@ -33,6 +42,7 @@ class BugerBuilder extends Component {
                 <Controls
                     ingredientAdded={this.addIngredientHandle}
                     ingredientRemoved={this.removeIngredientHandle}
+                    price={this.state.totalPrice}
                 />
             </div>
         );
