c88e8abda4570ef24a08ad34167c4f662a4cca69
Nazrul islam <jacksparrow.alex@gmail.com>
2021-02-28 15:56:40 +0600

    ingredient.js created but we will not keep this in manual mode, we will keep this in auto mode,

--- src/Components/BurgerBuilder/BugerBuilder.js
+++ src/Components/BurgerBuilder/BugerBuilder.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import Burger from "./Burger/Burger"
 class BugerBuilder extends Component {
     render() {
         return (
             <div>
-
+                <Burger />
             </div>
         );
     }
